f8c060aa9ed15874e8c5f7caebb4fcfc7159c337
huykuang18 <james.realcf@gmail.com>
2019-06-25 23:09:58 +0700

    Change welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,6 +91,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="register">Sign Up</a>
                     <a href="login">Login</a>
                 </div>
             </div>
